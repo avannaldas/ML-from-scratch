9efcc5ca04e08fa7afcd397ea2e5ec1a3ef4d1cf
Abhijit Annaldas <avannaldas@users.noreply.github.com>
2017-12-09 18:33:32 +0530

    Update cluster.py

--- avlearn/cluster.py
+++ avlearn/cluster.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.distance import euclidean
 
 class my_kmeans:
     centroids = []
@@ -17,18 +18,18 @@ class my_kmeans:
         while(itr < self.max_iters):
             # Assign Clusters
             self.clusters = [[] for _ in range(self.k)]
-            self.labels_ = np.array([i for i in range(len(X))])
+            self.labels_ = np.array([i+1 for i in range(len(X))])
             for x in range(len(X)):
-                clst_ind = np.argmin([self._square_distance(X[x], cntr)
+                clst_ind = np.argmin([self._euclidean_distance(X[x], cntr)
                                       for cntr in self.centroids])
                 self.labels_[x] = clst_ind
-                self.clusters[clst_ind].extend(X[x])
+                self.clusters[clst_ind].append([i for i in X[x]])
 
             # Update Centroids
             prev_centroids = self.centroids
             for i in range(self.k):
-                self.centroids[i][0] = np.mean(np.array(self.clusters[i][0]))
-                self.centroids[i][1] = np.mean(np.array(self.clusters[i][1]))
+                for j in range(len(self.clusters[i][0])):
+                    self.centroids[i][j] = np.mean([x[j] for x in self.clusters[i]])
 
             # Did centroids remain unchanged?
             if(np.array_equal(np.array(prev_centroids), np.array(self.centroids))):
@@ -39,8 +40,7 @@ class my_kmeans:
         return [self._predict_single(x) for x in X]
 
     def _predict_single(self, x):
-        return np.argmin([self._square_distance(x, c) for c in self.centroids])
+        return np.argmin([self._euclidean_distance(x, c) for c in self.centroids])
 
-    def _square_distance(self, x1, x2):
-        _dist = np.linalg.norm(x1 - x2)
-        return np.dot(_dist, _dist)
+    def _euclidean_distance(self, x1, x2):
+        return euclidean(x1, x2)
